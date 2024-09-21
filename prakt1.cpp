952eca68bd203a359b460131489f2eb2d13bde4c
Savostemko <zhusupova106@gmail.com>
2024-09-21 12:58:47 +0500

    last version

---
+++ prakt1.cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+float mysqrt(float a);
+int GCD(int firstNumber, int secondNumber);
+
+int main()
+{
+	//Задание 1 Вариант 7
+	cout << "task 1 option 7\n";
+
+	float number;
+	cin >> number;
+	cout << mysqrt(number) << endl;
+
+	//Задание 2 Вариант 22
+	cout << "task 2 option 22\n";
+	
+	int firstNumber, 
+		secondNumber, 
+		gcd = 1;
+
+	cin >> firstNumber;
+	cin >> secondNumber;
+
+	gcd = GCD(firstNumber, secondNumber);
+	cout << gcd;
+}
+
+float mysqrt(float a)
+{
+	float b = 2, c = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		c = 1./2 * (b + a / b);
+		b = c;
+	}
+	
+	return b;
+}
+
+int GCD(int firstNumber, int secondNumber) {
+	int gcd = 1;
+
+	for (int i = 1; i <= firstNumber || i <= secondNumber; i++) {
+		if (firstNumber % i == 0 && secondNumber % i == 0) {
+			gcd = i;
+		}
+	}
+
+	return gcd;
+}
